502dde4a9feacdaf1ee1bd072136449154751d71
Audrey Walters <walterae@rose-hulman.edu>
2019-03-19 21:26:27 -0400

    DONE.

--- src/problem1.py
+++ src/problem1.py
@@ -3,13 +3,13 @@ Exam 1, problem 1.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Audrey Walters.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
 # -----------------------------------------------------------------------------
-# TODO: 2. Right-click on the  src  folder and
+# DONE: 2. Right-click on the  src  folder and
 #              Mark Directory as ... Sources Root,
 #          if you have not already done so.
 # -----------------------------------------------------------------------------
@@ -118,9 +118,25 @@ def problem1(circle, rectangle, color, length, window):
       :type window:    rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.  SEE THE PICTURES in the PDF!
+    # DONE: 3. Implement and test this function.  SEE THE PICTURES in the PDF!
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    circle.attach_to(window)
+    rectangle.attach_to(window)
+    x2 = rectangle.get_center().x
+    y2 = rectangle.get_center().y
+    line1 = rg.Line(rg.Point(circle.center.x,circle.center.y),rg.Point(x2,y2))
+    line1.thickness = circle.outline_thickness
+    line1.color = color
+    y1 = line1.get_midpoint().y + 0.5*length
+    y2 = line1.get_midpoint().y - 0.5*length
+
+    line2 = rg.Line(rg.Point(line1.get_midpoint().x,y1),rg.Point(line1.get_midpoint().x,y2))
+    line2.color = circle.fill_color
+    line2.thickness = circle.outline_thickness + rectangle.outline_thickness
+    line1.attach_to(window)
+    line2.attach_to(window)
+    window.render()
 
 
 # -----------------------------------------------------------------------------
