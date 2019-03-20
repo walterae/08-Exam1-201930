038e33c320388d0b35be8bf0008988cdde556288
Audrey Walters <walterae@rose-hulman.edu>
2019-03-19 21:32:59 -0400

    DONE.

--- src/problem2.py
+++ src/problem2.py
@@ -3,8 +3,8 @@ Exam 1, problem 2.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Audrey Walters.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -15,7 +15,7 @@ def main():
 def test_factor_sum():
     """ Tests the   factor_sum   function. """
     ###########################################################################
-    #  TODO: 2. Implement this TEST function, as follows:
+    #  DONE: 2. Implement this TEST function, as follows:
     #
     #    1. Read the  doc-string of the   factor_sum   function defined below.
     #
@@ -45,6 +45,37 @@ def test_factor_sum():
     # WRITE YOUR TESTS BELOW HERE:
     ###########################################################################
 
+    # Test 1:
+    expected = 11  # This is the correct answer.
+    answer = factor_sum(28)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+
+    # Test 2:
+    expected = 4  # This is he correct answer.
+    answer = factor_sum(25)
+    print('Test 2 expected:', expected)
+    print('       actual:  ', answer)
+
+    # Test 3:
+    expected = 3  # This is the correct answer.
+    answer = factor_sum(6)
+    print('Test 3 expected:', expected)
+    print('       actual:  ', answer)
+
+    # Test 4:
+    expected = 4  # This is the correct answer.
+    answer = factor_sum(9)
+    print('Test 4 expected:', expected)
+    print('       actual:  ', answer)
+
+    # Test 5:
+    expected = 9  # This is the correct answer.
+    answer = factor_sum(10)
+    print('Test 5 expected:', expected)
+    print('       actual:  ', answer)
+
+
 
 def factor_sum(n):
     """
@@ -66,7 +97,7 @@ def factor_sum(n):
       -- and the sum of those numbers is   1 + 5 + 25,
              which is 31
       -- and the sum of the digits of 31 is 4,
-    so this function returns 4 when n is 28.
+    so this function returns 4 when n is 25.
 
        *** ASK FOR AN EXPLANATION IF YOU DO NOT UNDERSTAND THE ABOVE. ***
     """
